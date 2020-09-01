8be40b7b12bb7d7fb5d7e6e04190cf8974bf1a60
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-01 23:06:27 +0300

    feat: create basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AnnouncementForm } from './components/AnnouncementFrom';
+import { AnnouncementList } from './components/AnnouncementList';
+import { Announcement } from './components/Announcement';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      Clear
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Switch>
+        <Route path={'/'} exact component={AnnouncementList}/>
+        <Route path={'/create'} component={AnnouncementForm}/>
+        <Route path={'/announcement/:id'} component={Announcement}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
