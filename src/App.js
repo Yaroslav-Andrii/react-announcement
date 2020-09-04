6518468539a2217630a55aeaf9ea39881a279d5b
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 12:13:25 +0300

    feat: add logic to pages of type 404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { AnnouncementForm } from './components/AnnouncementFrom';
 import { AnnouncementList } from './components/AnnouncementList';
 import { Announcement } from './components/Announcement';
 import { NavBar } from './components/NavBar';
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path={'/'} exact component={AnnouncementList}/>
         <Route path={'/create'} component={AnnouncementForm}/>
         <Route path={'/announcement/:id'} component={Announcement}/>
+        <Route path={'*'} component={NotFoundPage}/>
       </Switch>
     </BrowserRouter>
   );
