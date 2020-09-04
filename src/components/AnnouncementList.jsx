472164099554cf87bce2a16a10011a06abc63765
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 12:50:08 +0300

    feat: create logic of AnnouncementList component work

--- src/components/AnnouncementList.jsx
+++ src/components/AnnouncementList.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {  useState } from 'react';
+import { useSelector } from 'react-redux';
 import { SearchForm } from './SearchForm';
+import { NavLink } from 'react-router-dom';
 
 export const AnnouncementList = () => {
-  return (
+  const announcements = useSelector(state => state.announcementState.announcements);
+  const [announcemetsList, setAnnouncemetsList] = useState(announcements);
+  
+  const searchHandler = searchString => {
+    setAnnouncemetsList(
+      announcements.filter(announcement => {
+        return ~announcement.title
+          .toLowerCase()
+          .indexOf(searchString.toLowerCase())
+      })
+    );
+  }
+
+  return (  
     <>
-      <SearchForm/>
-      <h3>
-        AnnouncementList
-      </h3>
+      <SearchForm filter={searchHandler}/>
+      <ul>
+        {
+          announcemetsList.length 
+          ?
+          announcemetsList.map(announcement => {
+              return (
+                <li key={announcement.id}>
+                  <NavLink to={`/announcement/${announcement.id}`}>{announcement.title}</NavLink>
+                </li>)
+            }) 
+          : 
+            "List of announcements is empty"
+        }
+      </ul>
     </>
   );
 }
