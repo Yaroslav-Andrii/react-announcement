cc38c991af43d79052bfa035537924bb59ab2070
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 13:29:13 +0300

    feat: add styles to SearchForm component

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export const SearchForm = ({filter}) => {
   return (
-    <form>
-      <input onChange={e => filter(e.target.value)} type="text" placeholder="find announcment"/>
+    <form className="mt-3 search-form">
+      <div className="form-group">
+      <input 
+        className="p-2"
+        id="serchInput" 
+        onChange={e => filter(e.target.value)} 
+        type="text" 
+        placeholder="What do you search?"
+      />
+      </div>
     </form>
   );
 }
