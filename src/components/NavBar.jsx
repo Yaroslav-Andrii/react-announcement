5c1de640ca204d7eef7da1ed1cb9ec0bafe874db
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 13:13:30 +0300

    feat: add styles to NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink
-          to="/"
-          exact
-        >
-          Announcements
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/create"
-        >
-          New announcement
-        </NavLink>
-      </li>
-    </ul>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <span className="navbar-brand">Announcements App</span>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <NavLink
+            className="nav-link"
+            activeClassName='active'
+            to="/"
+            exact
+          >
+            Announcements
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className="nav-link"
+            activeClassName='active'
+            to="/create"
+          >
+            New announcement
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
