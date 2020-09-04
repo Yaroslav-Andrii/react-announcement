790f7b494a3dfe255a153030d9c64ccd11138316
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 15:13:56 +0300

    Updates

---
+++ precache-manifest.3faec15dc23e694acf0a4fa7d86109a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fe1c2dff62a4667c456c8f48baf0e1",
+    "url": "/react-announcement/index.html"
+  },
+  {
+    "revision": "a5c1c77814abd617e2f9",
+    "url": "/react-announcement/static/css/main.7f9d9e3c.chunk.css"
+  },
+  {
+    "revision": "28ed7eae4f59b7c66769",
+    "url": "/react-announcement/static/js/2.c7f34bc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-announcement/static/js/2.c7f34bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c1c77814abd617e2f9",
+    "url": "/react-announcement/static/js/main.adce605d.chunk.js"
+  },
+  {
+    "revision": "11e33fd46bd78d3965fa",
+    "url": "/react-announcement/static/js/runtime-main.92c56012.js"
+  }
+]);
