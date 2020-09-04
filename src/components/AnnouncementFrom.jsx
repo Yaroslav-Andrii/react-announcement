4bae99ee72b41076517f85e2a3bd39a9b85d7f4f
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-04 12:50:30 +0300

    feat: create logic of AnnouncementFrom component work

--- src/components/AnnouncementFrom.jsx
+++ src/components/AnnouncementFrom.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addAnnouncement } from '../data/announcementsActions';
+import { Redirect } from 'react-router-dom';
 
 export const AnnouncementForm = () => {
+  const dispatch = useDispatch();
+
+  const [redirectState, setRedirectState] = useState(false);
+  const [formState, setFormChange] = useFormState();
+  
+  const [alert, setAlert] = useState('');
+  
+  function submitHandler(event) {
+    event.preventDefault();
+
+    if (formState.title && formState.description) {
+      dispatch( addAnnouncement(formState) );
+      setRedirectState(true);
+    } else {
+      setAlert('Some field is empty! Fill all fields for create announcement');
+    }
+  }
+
   return (
-    <h3>
-      AnnouncementForm
-    </h3>
+    <>
+    { redirectState && <Redirect to="/"/> }
+    <h3>Create new announcement!</h3>
+
+    { alert ? <p>{alert}</p> : null}
+    <form onSubmit={submitHandler}>
+      <label>
+        Title
+        <input 
+          onChange={setFormChange} 
+          id="title" 
+          type="text" 
+          value={formState.title}
+        />
+      </label>
+      <label>
+        Description
+        <textarea 
+          onChange={setFormChange} 
+          id="description"
+          value={formState.description}
+        ></textarea>
+      </label>
+      <button type="submit">Add announcement</button>
+    </form>
+    </>
   );
+}
+
+function useFormState() {
+  const initialFormState = {title: '', description: ''};
+  const [formState, setFromState] = useState(initialFormState);
+  function inputChange(event) {
+    setFromState({
+      ...formState, 
+      [event.target.id]: event.target.value
+    });
+  }
+
+  return [formState, inputChange];
 }
